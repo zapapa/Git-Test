6f03be0040d686664dd34985a08d6f143220c349
zapapa <zapapa@qq.com>
2021-09-16 09:26:57 +0800

    hot-fix commit2

--- src/main/java/com/zapapa/git/GitTest.java
+++ src/main/java/com/zapapa/git/GitTest.java
@@ -2,8 +2,9 @@ package com.zapapa.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello hot-fix");
         System.out.println("hello git2!");
         System.out.println("hot-fix test");
+        System.out.println("123123");
     }
 }
