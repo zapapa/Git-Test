08c78706cb3aae3b34f69e005fdf32502c85f00e
zapapa <zapapa@qq.com>
2021-09-16 09:25:53 +0800

    master commit1

--- src/main/java/com/zapapa/git/GitTest.java
+++ src/main/java/com/zapapa/git/GitTest.java
@@ -2,7 +2,7 @@ package com.zapapa.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello!");
         System.out.println("hello git2!");
         System.out.println("hot-fix test");
     }
